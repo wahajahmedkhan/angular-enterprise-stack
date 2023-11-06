cfb04adc20ebc2d4f60e384ac99687a123c1844c
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2023-11-06 01:18:25 +0100

    deps: updated linting rules

--- apps/forecast-fusion/src/app/app.config.ts
+++ apps/forecast-fusion/src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { routerReducer } from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideStore({ router: routerReducer }),
+    provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })],
 };
