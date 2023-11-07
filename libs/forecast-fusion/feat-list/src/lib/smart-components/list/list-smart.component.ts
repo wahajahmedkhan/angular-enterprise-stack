d187abbbee5b81ce904e8a1e24d88df5e5c32ee7
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2023-11-07 05:53:39 +0100

    feat(ff): finalized

--- libs/forecast-fusion/feat-list/src/lib/smart-components/list/list-smart.component.ts
+++ libs/forecast-fusion/feat-list/src/lib/smart-components/list/list-smart.component.ts
@@ -1,5 +1,8 @@
-import { selectForecastData } from '@angular-enterprise-stack/forecast-fusion/data-access';
-import { DatePipe, NgForOf } from '@angular/common';
+import {
+  selectForecastData,
+  selectForecastStatus,
+} from '@angular-enterprise-stack/forecast-fusion/data-access';
+import { DatePipe, NgForOf, NgIf, NgStyle } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
@@ -7,13 +10,17 @@ import { Store } from '@ngrx/store';
 @Component({
   selector: 'ff-feat-list',
   standalone: true,
-  imports: [DatePipe, NgForOf],
+  imports: [DatePipe, NgForOf, NgStyle, NgIf],
   templateUrl: './list-smart.component.html',
   styleUrls: ['./list-smart.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListSmartComponent {
   private readonly store = inject(Store);
+
+  public readonly forecastStatus = toSignal(
+    this.store.select(selectForecastStatus),
+  );
   public readonly forecastData = toSignal(
     this.store.select(selectForecastData),
     { initialValue: [] },
