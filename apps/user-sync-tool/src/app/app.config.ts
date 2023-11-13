c69a6df347f9c1df353070ed9ebff087baa9af3e
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2023-11-13 03:34:17 +0100

    feat(ust-app): Updated the app and routed to shell

--- apps/user-sync-tool/src/app/app.config.ts
+++ apps/user-sync-tool/src/app/app.config.ts
@@ -1,10 +1,19 @@
+import { debug } from '@angular-enterprise-stack/shared/ngrx';
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { provideStore } from '@ngrx/store';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideStore({ router: routerReducer }, { metaReducers: [debug()] }),
+    provideRouterStore(),
+    provideHttpClient(),
+  ],
 };
--- apps/user-sync-tool/src/app/app.routes.ts
+++ apps/user-sync-tool/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { ustFeatShellRoutes } from '@angular-enterprise-stack/user-sync-tool/feat-shell';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = ustFeatShellRoutes;
