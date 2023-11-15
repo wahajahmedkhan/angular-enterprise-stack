45b17628f5fde5c2bc2b6ef9ea174c9a0534b6d3
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2023-11-15 02:36:01 +0100

    refactor(ust-types): Removed unused Alerts

--- libs/user-sync-tool/types/src/lib/status.types.ts
+++ libs/user-sync-tool/types/src/lib/status.types.ts
@@ -1,13 +1,11 @@
 export type StatusTypes = 'idle' | 'pending' | 'success' | 'error';
 
 export enum SuccessMessages {
-  RANDOM_USER_SUCCESS = 'Random user added successfully',
-  USER_ADDED_TO_FAVOURITE_SUCCESS = 'User successfully added to favourite lise',
+  USER_ADDED_TO_FAVOURITE_SUCCESS = 'User successfully added to favourite list',
 }
 
 export enum ErrorMessages {
   INTERNAL_SERVER_ERROR = 'Internal server error',
-  RANDOM_USER_ERROR = 'Random user not added',
   MAXIMUM_FAVOURITE_USER_ERROR = 'Maximum favourite users selected. Delete some to add more',
   FAVOURITE_USER_EXIST_ERROR = 'This user is already a favourite user',
 }
