605300cd9099adfc7927a060ed12690305f22612
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2023-11-15 01:04:17 +0100

    style(ust-types):Updated typings

--- libs/user-sync-tool/types/src/lib/status.types.ts
+++ libs/user-sync-tool/types/src/lib/status.types.ts
@@ -8,5 +8,5 @@ export enum SuccessMessages {
 export enum ErrorMessages {
   INTERNAL_SERVER_ERROR = 'Internal server error',
   RANDOM_USER_ERROR = 'Random user not added',
-  USER_ADDED_TO_FAVOURITE_ERROR = 'User successfully added to favourite lise',
+  USER_ADDED_TO_FAVOURITE_ERROR = 'Maximum favourite users selected. Delete some to add more',
 }
