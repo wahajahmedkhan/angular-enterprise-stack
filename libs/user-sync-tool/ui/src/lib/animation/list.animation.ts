a11ba024dd2e74d13570ea88ff106b067a5177ab
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2023-11-15 01:28:27 +0100

    feat(ust-ui): Added parameters to animation

--- libs/user-sync-tool/ui/src/lib/animation/list.animation.ts
+++ libs/user-sync-tool/ui/src/lib/animation/list.animation.ts
@@ -7,31 +7,32 @@ import {
   trigger,
 } from '@angular/animations';
 
-export const listAnimation = trigger('listAnimation', [
-  transition('* => *', [
-    query(
-      ':enter',
-      [
-        style({ opacity: 0, transform: 'translateX(-15px)' }),
-        stagger(
-          '0ms',
-          animate(
-            '600ms ease-out',
-            style({ opacity: 1, transform: 'translateX(0px)' }),
+export const listAnimation = (duration = '600ms') =>
+  trigger('listAnimation', [
+    transition('* => *', [
+      query(
+        ':enter',
+        [
+          style({ opacity: 0, transform: 'translateX(-15px)' }),
+          stagger(
+            '0ms',
+            animate(
+              `${duration} ease-out`,
+              style({ opacity: 1, transform: 'translateX(0px)' }),
+            ),
           ),
-        ),
-      ],
-      { optional: true },
-    ),
-    query(
-      ':leave',
-      [
-        animate(
-          '600ms ease-in',
-          style({ opacity: 0, transform: 'translateX(15px)' }),
-        ),
-      ],
-      { optional: true },
-    ),
-  ]),
-]);
+        ],
+        { optional: true },
+      ),
+      query(
+        ':leave',
+        [
+          animate(
+            `${duration} ease-in`,
+            style({ opacity: 0, transform: 'translateX(15px)' }),
+          ),
+        ],
+        { optional: true },
+      ),
+    ]),
+  ]);
